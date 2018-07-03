70e61f4874b9aa7d0dee888a55a5819f0a4bc089
pimink <jjbdamien@gmail.com>
2018-07-03 19:18:08 +0200

    ajout d'une classe abstraite pour touts les objets (icompris le vide et les plateformes (héritage...)

---
+++ Project5/objet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Project5
+{
+    namespace objet
+    {
+        abstract class objet
+        {
+            private Color[,] PFrame;
+            private Bitmap Frame;
+            public Color GetPixel(int x, int y)
+            {
+                return PFrame[x, y];
+            }
+            private void chargerFrame()
+            {
+                for(int i = 0; i < Frame.Height; i++)
+                    for(int o = 0; o< Frame.Width; i++)
+                    {
+                        PFrame[i,o] = Frame.GetPixel(i,o);
+                    }
+            }
+        }
+    }
+}
